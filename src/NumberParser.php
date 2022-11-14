934aab982e8d0cecb5d3287d10763282a7f59588
Artem <artem@agsys.ru>
2022-11-14 16:41:59 +0300

    init commit

--- src/NumberParser.php
+++ src/NumberParser.php
@@ -7,7 +7,6 @@ namespace TemaGo\OtusTest;
 class NumberParser
 {
     function parse($text) {
-        $number = preg_replace('/[^0-9]/', '', $text);
-        return $number;
+        return preg_replace('/[^0-9]/', '', $text);
     }
 }
